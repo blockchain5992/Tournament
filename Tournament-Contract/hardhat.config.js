9133e2229292d69441283b4498cb58e6db7c7801
blockchain5992 <ayushporwal91@gmail.com>
2023-04-24 17:08:36 +0530

    feat: added networks

--- Tournament-Contract/hardhat.config.js
+++ Tournament-Contract/hardhat.config.js
@@ -1,6 +1,32 @@
 require("@nomicfoundation/hardhat-toolbox");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  networks: {
+    hardhat: {
+      allowUnlimitedContractSize: false,
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545", // same address and port for both Buidler and Ganache node
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+      gas: 8000000,
+    },
+    goerli: {
+      url: `${process.env.RPC_URL}`,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+      throwOnTransactionFailures: true,
+      loggingEnabled: true,
+      gas: 8000000,
+      blockGasLimit: 8000000,
+    },
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
