1d79490484aab3fe2e58d7fd96fa77a100f62350
blockchain5992 <ayushporwal91@gmail.com>
2023-04-24 17:08:50 +0530

    fix: deployement script

--- Tournament-Contract/scripts/deploy.js
+++ Tournament-Contract/scripts/deploy.js
@@ -7,21 +7,12 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const Tournament = await hre.ethers.getContractFactory("Tournament");
+  const tournament = await Tournament.deploy();
 
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
+  await tournament.deployed();
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  console.log("Contract deployed Address:", tournament.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
